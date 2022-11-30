8d4d65dc7aa095ee96b6aa093ce65a98c7f3cd50
amaljom <jomonamal999@gmail.com>
2022-11-30 17:51:15 +0100

    first commit

---
+++ src/main/java/org/generation/db/Main.java
@@ -0,0 +1,57 @@
+package org.generation.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		String url = "jdbc:mysql://localhost:3306/nations";
+		String user = "root";
+		String password = "root";
+		 
+		Connection con = null;
+		try {
+			
+			con = DriverManager.getConnection(url, user, password);
+			
+			final String sql = " SELECT countries.name, countries.country_id, regions.name, continents.name\r\n"
+							+  " FROM nations.countries\r\n"
+							+  "	JOIN regions\r\n"
+							+  "		ON countries.region_id = regions.region_id\r\n"
+							+  "	JOIN continents\r\n"
+							+  "		ON regions.continent_id = continents.continent_id\r\n"
+							+  " ORDER BY countries.name ";
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				final String name = rs.getString(1);
+				final int id = rs.getInt(2);
+				final String regionName = rs.getString(3);
+				final String continentName = rs.getString(4);
+				
+				System.out.println(id + " - " + name + " - " + regionName + " - " + continentName);
+			}
+			
+			ps.close();
+			
+		} catch (SQLException ex) {
+			
+			ex.printStackTrace();
+		} finally{
+			
+			try {
+				con.close();
+			} catch (Exception e) { }
+		}
+	}
+}
