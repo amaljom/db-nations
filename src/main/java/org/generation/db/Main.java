08f6b55d6299566240c793a4e08a9a64e9b7d1da
amaljom <jomonamal999@gmail.com>
2022-12-01 11:36:11 +0100

    research by id implemented

--- src/main/java/org/generation/db/Main.java
+++ src/main/java/org/generation/db/Main.java
@@ -51,6 +51,44 @@ public class Main {
 			
 			ps.close();
 			
+			// ****************************
+			con = DriverManager.getConnection(url, user, password);
+			final String sql2 = "SELECT DISTINCT languages.language, country_stats.population, country_stats.gdp, country_stats.year\r\n"
+							+ "FROM nations.country_stats\r\n"
+							+ "	JOIN countries\r\n"
+							+ "		ON country_stats.country_id = countries.country_id\r\n"
+							+ "	JOIN country_languages\r\n"
+							+ "		ON countries.country_id = country_languages.country_id\r\n"
+							+ "	JOIN languages\r\n"
+							+ "		ON country_languages.language_id = languages.language_id\r\n"
+							+ "WHERE countries.country_id = ? \r\n"
+							+ "ORDER BY country_stats.year DESC LIMIT 1";
+	
+			PreparedStatement ps1 = con.prepareStatement(sql2);
+			
+			
+			System.out.print("inserisci L'ID del Paese da ricercare: ");
+			String nationId = sc.nextLine();
+			
+			int id = Integer.parseInt(nationId);
+			ps1.setInt(1, id);
+			
+			
+			ResultSet rs1 = ps1.executeQuery();
+			
+			while(rs1.next()) {
+				
+				final String language = rs1.getString(1);
+				final int population = rs1.getInt(2);
+				final String gdp = rs1.getString(3);
+				final int year = rs1.getInt(4);
+				
+				System.out.println(language + " - " + population + " - " + gdp + " - " + year);
+			}
+			
+			ps1.close();
+			
+			
 		} catch (SQLException ex) {
 			
 			ex.printStackTrace();
